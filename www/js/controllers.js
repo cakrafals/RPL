5f4b1258db2e70888eb9dd97ed6168a84d2abccf
cakrafals <cakrafals@gmail.com>
2016-10-06 00:37:53 +0700

    ini baru awal bro besok lanjut lagi

---
+++ www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', ['$scope', '$stateParams', 
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('helpCtrl', ['$scope', '$stateParams', 
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('aboutCtrl', ['$scope', '$stateParams', 
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('menuCtrl', ['$scope', '$stateParams', 
+function ($scope, $stateParams) {
+
+
+}])
+ 
